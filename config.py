b59005da292d7c42fa39d051e5717b77d675d96f
sooooner <tnsgh0101@gmail.com>
2021-02-13 15:12:53 +0900

    add config

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
-MODEL_NAME = 'Residual Attention Network'
-
+# model parameter
 p = 1
 t = 2
 r = 1
+MODEL_NAME = 'Residual Attention Network'
 
+# training parameter
+LEARNING_RATE = 0.1
+DECAY = 1e-4
+MOMENTUM = 0.9
 EPOCHS = 100
-BATCH_SIZE = 64
+BATCH_SIZE = 128
